27be4beadd6782761b57c00f9c7518ab9d1a6c97
Christopher Barr <chris.barr@amwater.com>
2022-08-02 08:16:39 -0400

    add auto import and auto import components

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,9 +1,25 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
+import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      dts: true,
+      directoryAsNamespace: true,
+    }),
+    AutoImport({
+      imports: ['vue'],
+      dts: true,
+      eslintrc: {
+        enabled: true,
+      },
+      vueTemplate: true,
+    }),
+  ],
   base: '',
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],

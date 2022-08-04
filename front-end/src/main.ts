cd1ee09ecd3b8e48acf164c6a1e7debcc99d78ce
Christopher Barr <chris.barr@amwater.com>
2022-08-04 18:48:04 -0400

    lets really remove vue-query for real for real this time

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -1,8 +1,6 @@
 import App from './App.vue';
 import { createApp } from 'vue';
-import { VueQueryPlugin } from 'vue-query';
 
 const app = createApp(App);
 
-app.use(VueQueryPlugin);
 app.mount('#app');

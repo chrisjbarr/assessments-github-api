fbe4bd5f3332ab76e79cc4bcb9bae58c4b5c4a23
Christopher Barr <chris.barr@amwater.com>
2022-08-04 06:31:42 -0400

    note about ci

--- front-end/test/failing.test.ts
+++
@@ -1,9 +0,0 @@
-/**
- * taken from: https://github.com/vitest-dev/vitest/blob/main/examples/basic/test/suite.test.ts
- */
-
-describe('intentionally fail this test', () => {
-  it('should fail and display as a failure in github', () => {
-    assert.equal('should fail', 'will fail');
-  });
-});
